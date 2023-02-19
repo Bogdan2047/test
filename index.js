0ce6c7dcf33d7b5115ad6e43efd72a3078829d5e
Bogdan2047 <81803193+Bogdan2047@users.noreply.github.com>
2023-02-19 13:36:15 +0200

    the end

--- index.js
+++ index.js
@@ -1,268 +1,149 @@
-document.getElementById('but1').addEventListener('click', add);
-
-const del = document.getElementById('but2');
+let DB = JSON.parse(localStorage.getItem('DB')) || []
 
 
-let LS = localStorage;
+const initDb = () => {
 
-let name = document.querySelector('.name');
-let p = document.querySelector('.p');
+    const DB_KEY = 'DB';
+    const getAllPosts = () => {
+        let localStorageItems = localStorage.getItem(DB_KEY)
+        if (localStorageItems) {
+            DB = JSON.parse(localStorageItems);
 
-let one = document.querySelector('.one');
-let two = document.querySelector('.two');
+            return DB
+        }
+        return []
+    }
+    return {
+        getAllPosts
+    }
+};
 
-let dates = document.querySelector('.dates');
+const initRender = () => {
 
-let post = document.getElementById('posts');
+    const postsListContainer = document.getElementById('postsLists');
+    const renderOnePost = (post) => {
+        const mainDiv = document.createElement('div');
+        mainDiv.classList = 'post-block'
+        const postDiv = document.createElement('div');       
+        postDiv.classList = 'post-body'
+        const title = document.createElement('h3');
+        title.innerHTML = post.title;
+        const content = document.createElement('p');
+        const addcom = document.createElement('button')
+        addcom.classList = 'btn btn-success'
+        addcom.innerHTML = 'add comment';
+        content.innerHTML = post.content;
+        postDiv.appendChild(title);
+        postDiv.appendChild(content);
+        postDiv.appendChild(addcom);
+        mainDiv.appendChild(postDiv)
+        postsListContainer.appendChild(mainDiv);
 
-let id = Date.now();
-
-let addCom = document.getElementById('add');
-
-let coment = document.getElementById('coment');
-
-let info = [];
-
-// let getId;
-
-let BD = [];
-
-
-function add () {
-
-    let input = document.querySelector('.input');
-    input.addEventListener('blur', text);
-    
-    let textarea = document.querySelector('.textarea');
-    textarea.addEventListener('blur', text);
-    
-
-    function text (event) {
        
-    }
-
-
-    if(input.value == '' && textarea.value !== ''){
-        one.innerHTML = 'заполните поле';
-        two.innerHTML = ' ';
-
-    }
-
-    if(input.value !== '' && textarea.value == ''){
-        one.innerHTML = ' '
-        two.innerHTML = 'заполните поле';
-    }
-
-    if(input.value == '' && textarea.value == ''){
-        one.innerHTML = 'заполните поле';
-        two.innerHTML = 'заполните поле';
-    }
-
-    if(input.value !== '' && textarea.value !== ''){
-        one.innerHTML = ' ';
-        two.innerHTML = ' ';
-
-        let posts = [{
-            title: input.value,
-            content: textarea.value,
-            id: id
-        }]
-
-        info.push(posts)
-
-        LS.setItem('posts', JSON.stringify(info));
-    
-        info.splice(0, info.length);
-
-        let a = JSON.parse(LS.getItem('posts', info));
-
-        info.push(...a);
-
-        post.innerHTML = info.map((item) => {
-
-            let g = [];
-
-            item.forEach((item) => {
-                g = item;   
-                getId = item.id
-
-            });
-
-            return `
-                    <div class="post" id="${g.id}">
-                        <div>
-                            <h3 class="name">${g.title}</h3>
-                            <p class="p">${g.content}</p>
-                        </div>
-                        <div>
-                            <button class='comments'  id="${g.id}">add comments</button>
-                            <button class='hide'  id="${g.id}">hide comments</button>
-                        </div>
-                    </div>
-                   `
-                   
-        }).join('').slice()
-
-        input.value = '';
-        textarea.value = '';
-
-        
-        document.location.reload();             
-    }
-}   
-
-
-let aa = [];
-
-let getId;
-
-let end = [];
-
-let ones;
-let twos;
-
-window.onload = function(){
-
-
-    if(LS.getItem('posts') !== null){
-        let b = JSON.parse(LS.getItem('posts', info))
-        info.push(...b);
-        post.innerHTML = info.map((item) => {
-            let r = [];
-
-            item.forEach((item) => {
-                r = item;  
-                getId = item.id 
-                ones = item;
-            });
-            return `
-                <div class="post" id="${r.id}">
-                    <div>
-                        <h3 class="name">${r.title}</h3>
-                        <p class="p">${r.content}</p>
-                    </div>
-                    <div class='action'>
-                        <button class='comments' id=''${r.id}>add comments</button>
-                        <button class='hide' id=''${r.id}>hide comments</button>
-                    </div>
-                </div>
-                   `
-            }).join('').slice()
-        }
-
-
-let comments = document.getElementsByClassName('comments');
-
-let numComments = comments.length;
-
-for (var i = 0; i < numComments; i++) {
-    comments[i].addEventListener('click', openList, false);
-  }
-
-
-let hide = document.getElementsByClassName('hide');
-
-for (var i = 0; i < numComments; i++) {
-    hide[i].addEventListener('click', hideList, false);
-  }
-
-function empty () {
-    coment.innerHTML = '';
-}
-
-function hideList () { 
-    empty()      
-}
-  
-function openList (event) {
-
-coment.innerHTML = ''
-   post.innerHTML = `
-   <div class="post" id="${ones.id}">
-        <div>
-            <h3 class="name">${ones.title}</h3>
-            <p class="p">${ones.content}</p>
-        </div>
-        <div class='action'>
-            <button class='comments' id=''${ones.id}>add comments</button>
-            <button class='hide' id=''${ones.id}>hide comments</button>
-        </div>
-    </div>
-    <div id='before' class='first' id="${getId}">
-        <textarea class='comm' placeholder='writing your comment'></textarea>
-         <button class='send'>add comment</button>
-    </div>
-   `
-   
-    let textCom = document.querySelector('.comm');
-    textCom.addEventListener('blur', putUp);
-
-    
-    function putUp(event) {
-
-        if(textCom.value !== ''){
-            let bb = [{
-                text:  textCom.value,
-                id: getId
-            }];
-
-            aa.push(bb);
-
-            LS.setItem('comments',JSON.stringify(aa));
-
-            let we = JSON.parse(LS.getItem('comments', aa));
-
-            aa.splice(0, aa.length);
-
-            aa.push(...we);
-
-            coment.innerHTML = aa.map((item) => {
-                let c = [];
-                item.forEach((item) =>{
-                    c = item;
-                })
-            return `
-              <div class='this' id='${c.id}'>
-                  <p>${c.text}</p>
-              </div>
-            `
-            }).join('').slice();
-    
-            textCom.value = '';
-            document.location.reload();           
-        }
-
-    }
-}
-
-function commentComponent (t) {
-    return `
-     <div class='this' id='${t.id}'>
-         <p>${t.text}</p>
-     </div>
- `   
-}
-
-
-if(LS.getItem('comments') !== null){
-    let see = JSON.parse(LS.getItem('comments', aa));
-    
-        aa.splice(0, aa.length);
-    
-        aa.push(...see);
-    
-        coment.innerHTML = aa.map((item) => {
-            let t = [];
-            
-            item.forEach((item) =>{
-                t = item;
-                twos = item;
+        if(post.comments){
+            let arr = post.comments;
+            arr.map((item) => {
+                const postCom = document.createElement('div');
+                postCom.classList = 'com-block'
+                const comment = document.createElement('p');
+                comment.classList = 'com-text'
+                comment.innerHTML = item
+                postCom.appendChild(comment)
+                mainDiv.appendChild(postCom)
+                postsListContainer.appendChild(mainDiv)
             })
-            return commentComponent(t)
+            
+        }
+       
+        
+        formListenerSend(addcom,postsListContainer, mainDiv, post);
+        
+    }
 
-        }).join('').slice()
-    
+
+
+    const formListenerSend = (addcom,postsListContainer,mainDiv, post) => {
+        
+        function inputText () {
+                const textAreaDiv = document.createElement('div');
+                textAreaDiv.classList = 'add-com'
+                const textarea = document.createElement('textarea');
+                textarea.classList = 'textarea'
+                const send = document.createElement('button');
+                send.classList = 'resp'
+                send.innerHTML = 'send';
+                textAreaDiv.appendChild(textarea);
+                textAreaDiv.appendChild(send);
+                mainDiv.appendChild(textAreaDiv);
+                postsListContainer.appendChild(mainDiv);
+               
+
+                formListenerCom(send,textarea,post)
+
+        }
+
+        addcom.addEventListener('click', inputText);
+    }
+
+
+            const formListenerCom = (send,textarea, post) => {
+
+            function sendCom () {
+                if(!textarea.value == ''){
+                    let a = textarea.value
+
+                    DB.forEach((item) => {
+                        if(item.id === post.id){
+                            item.comments.push(a);                   
+                        }
+                    })
+
+                    let newDB = DB;
+                    localStorage.setItem('DB', JSON.stringify(newDB));
+                    document.location.reload(); 
+                    DB.push(JSON.parse(localStorage.getItem('DB')))
+                }
+
+            }
+
+                send.addEventListener('click', sendCom)
+            }
+
+
+    const renderExistsPosts = () => {
+        const posts = db.getAllPosts()
+        posts.map(post => {
+            renderOnePost(post)
+        })
+    }
+    const formListener = () => {
+        
+        document.querySelector('button').addEventListener('click', (event) => {
+            let title = document.getElementById("postTitle").value;
+            let desc = document.getElementById("postDesc").value;
+
+            if(!title == '' && !desc == ''){
+                let blog = {id: Date.now(), title: title, content: desc, comments: []}
+                DB.push(blog);
+                localStorage.setItem('DB', JSON.stringify(DB));
+                document.location.reload(); 
+            }
+  
+        })
+    }
+
+
+    formListener()
+
+    return  {
+        renderExistsPosts,
+
     }
 }
 
-   
+
+const db = initDb();
+const render = initRender();
+
+
+render.renderExistsPosts();
